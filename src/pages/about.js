eeadca3897e95166d2171db55f3bdde85824830c
fj <castelloiv@gmail.com>
2020-04-07 11:00:42 -0400

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,8 @@
 import React from "react"
-
+import Layout from '../components/layout'
 export default () => (
-    <div>
+    <Layout>
         <h1>Helllooo</h1>
         <p>This is a test</p>
-    </div>
+    </Layout>
 )
