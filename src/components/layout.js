ce5d410819477a405394d9dd2cd1bed0505164dc
fj <castelloiv@gmail.com>
2020-04-07 10:37:33 -0400

    Stlyes

---
+++ src/components/layout.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default ({ children }) => (
+    <div style={{ margine: `erem auto`, maxWidth: 650, padding: `0 1rem`}}>
+        {children}
+    </div>
+) 
---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <div>
+        <h1>Helllooo</h1>
+        <p>This is a test</p>
+    </div>
+)
