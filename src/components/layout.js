e4152ae3d62dcd603c1cb793e96068dd6f0a477a
fj <castelloiv@gmail.com>
2020-04-14 02:26:56 -0400

    Global

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 import Main from './mainstyle.module.scss'
-import Dark from "./darkmode.js"
+import useDarkMode from 'use-dark-mode';
+
 const ListLink = props => (
     <li style={{ display: `inline-block`, marginRight: `1rem` }}>
         <Link to={props.to}>{props.children}</Link>
     </li>
     
 )
-export default ({ children }) => (
-    <div>
+
+
+const Layout = ({ children }) => {
+
+    const darkMode = useDarkMode(false);
+
+    return (
+
+        
+        <div>
     <div style={{ margin: `0 auto`, maxWidth: 800, padding: `0 1rem`, }}>
         <header style={{ marginBottom: `1.5rem`}}>
             <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, }}><h3 style={{ display: `inline`, color: `#6246ea`}}>GRIND</h3></Link>
@@ -18,7 +27,18 @@ export default ({ children }) => (
                 <ListLink to="/" style={{  color: `#6246ea` }} >Home</ListLink>
                 
                 <ListLink to="/identityandaccessmanagement" className={Main.linkc} >4.0</ListLink>
-            <Dark />
+                <div>
+      <button type="button" onClick={darkMode.disable}>
+        ☀
+      </button>
+     
+      <button type="button" onClick={darkMode.enable}>
+        ☾
+      </button>
+    </div>
+                <div>
+      
+    </div>
             </ul>
             
         </header>
@@ -30,4 +50,10 @@ export default ({ children }) => (
     <footer className={Main.footer}>No pop ups. No cookies. No ads. <br></br>We don't do those.<br></br>
     Meme quality not guaranteed. </footer>
     </div>
-) 
+
+    )
+  
+    
+}
+
+export default Layout
