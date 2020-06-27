a19cfdeda8bfccdf2be43564dcda49d1917f1cf9
fj <castelloiv@gmail.com>
2020-06-26 23:14:44 -0400

    Spelling errors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import Style from "./navbar.module.scss"
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Shockedp from "../images/relong2.jpg"
-import useDarkMode from 'use-dark-mode';
+import useDarkMode from "use-dark-mode"
 import Toggle from "./toggle"
-import Darkmodetoggle from './darkmode'
-
-
+import Darkmodetoggle from "./darkmode"
 
 const Navlink = props => (
-   
-
-
-    
-        
-        <AniLink cover bg={`
+  <AniLink
+    cover
+    bg={`
 
     url(${Shockedp})
     center / cover   /* position / size */
@@ -23,42 +18,54 @@ const Navlink = props => (
     padding-box      /* origin */
     content-box      /* clip */
     white            /* color */
-  `}  to={props.to}>
-
-{props.children}
-</AniLink>
-    
-    
+  `}
+    to={props.to}
+  >
+    {props.children}
+  </AniLink>
 )
 
-
-
 function Navbar() {
-    const [navnames, setNavnames] = useState(false)
-    const darkMode = useDarkMode(false);
-    
+  const [navnames, setNavnames] = useState(false)
+  const darkMode = useDarkMode(false)
 
-    return (
-        <div className={Style.container}>
-                
-               
-    
-        <h3><Navlink to="/">GRIND</Navlink></h3>
-        <Darkmodetoggle />
-        <nav className={`${Style.mobile} ${navnames ? Style.showmenu : Style.hidemenu}`}>
-            <ul>
-                <li onClick={() =>  setNavnames(false) }><Navlink to="/">Home</Navlink></li>
-                <li><Navlink to="/memes">Memes</Navlink></li>
-                <li ><Navlink to="/lobbying">Lobbying</Navlink></li>
-                <li ><Navlink to="/about">About</Navlink></li>
-            </ul>
-        </nav>
+  return (
+    <div className={Style.container}>
+      <h3>
+        <Navlink to="/">GRIND</Navlink>
+      </h3>
+      <Darkmodetoggle />
+      <nav
+        className={`${Style.mobile} ${
+          navnames ? Style.showmenu : Style.hidemenu
+        }`}
+      >
+        <ul>
+          <li onClick={() => setNavnames(false)}>
+            <Navlink to="/">Home</Navlink>
+          </li>
+          <li>
+            <Navlink to="/memes">Memes</Navlink>
+          </li>
+          <li>
+            <Navlink to="/lobbying">Lobbying</Navlink>
+          </li>
+          <li>
+            <Navlink to="/about">About</Navlink>
+          </li>
+        </ul>
+      </nav>
 
-        <button className={Style.navbutton} onClick={() =>  setNavnames(!navnames)}>
-            <span className={`${Style.navspan} ${navnames ? Style.animate : null} `}></span>
-        </button>
-        </div>
-    )
+      <button
+        className={Style.navbutton}
+        onClick={() => setNavnames(!navnames)}
+      >
+        <span
+          className={`${Style.navspan} ${navnames ? Style.animate : null} `}
+        ></span>
+      </button>
+    </div>
+  )
 }
 
 export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,118 +2,130 @@ import React, { Fragment } from "react"
 import Info from "../components/info"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-import Pandasvg from '../images/panda.svg'
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Pandasvg from "../images/panda.svg"
+
+import Footer from "../components/footer"
 import Move from "../components/move"
 import Navbar from "../components/navbar"
 
-import { Link } from "gatsby"
-import Main from '../components/mainstyle.module.scss'
-const Panda = () => <><img className={Main.panda} style={{ height: `200px`, marginBottom: `10px`}} src={Pandasvg} /></>
+import Main from "../components/mainstyle.module.scss"
+const Panda = () => (
+  <>
+    <img
+      className={Main.panda}
+      style={{ height: `200px`, marginBottom: `10px` }}
+      src={Pandasvg}
+    />
+  </>
+)
 const Index = props => (
   <Fragment>
     <Info title="Shocked Security Koala" />
     <Navbar />
     <section className={Main.title}>
-    <h1 className={Main.topmargin}>CompTIA Security<span style={{ color: `#f11c59`}}>+</span> </h1>
-    <h4 className={Main.lowmargin}>Misery. Memes. Mastery.</h4>
+      <h1 className={Main.topmargin}>
+        CompTIA Security<span style={{ color: `#f11c59` }}>+</span>{" "}
+      </h1>
+      <h4 className={Main.lowmargin}>Misery. Memes. Mastery.</h4>
     </section>
     <div className={Main.crossline}>
-    <div className={Main.pandafloat}>
-    <Panda />
-
+      <div className={Main.pandafloat}>
+        <Panda />
+      </div>
     </div>
-    </div>
-   
 
     <div className={Main.container}>
-    
       <div className={Main.fademefam}>
-          <div className={Main.flexc}>
-            <h4>Section 1.0</h4>
-          </div>
-          <div className={Main.flexc}>
-            <h4>Section 2.0</h4>
-          </div>
-          <div className={Main.flexc}>
-            <h4>Section 3.0</h4>
-          </div>
-          <div className={Main.flexc}>
-            <h4><Move to="/identityandaccessmanagement">4.0 Identity and Access Management</Move></h4>
-          </div>
-          <div className={Main.flexc}>
-            <h4>Section 5.0</h4>
-          </div>
-          <div className={Main.flexc}>
-          <h4><Move to="/cryptography">6.0 Cryptography and PKI</Move></h4>
-          </div>
-
+        <div className={Main.flexc}>
+          <h4>Section 1.0</h4>
+        </div>
+        <div className={Main.flexc}>
+          <h4>Section 2.0</h4>
+        </div>
+        <div className={Main.flexc}>
+          <h4>Section 3.0</h4>
+        </div>
+        <div className={Main.flexc}>
+          <h4>
+            <Move to="/identityandaccessmanagement">
+              4.0 Identity and Access Management
+            </Move>
+          </h4>
+        </div>
+        <div className={Main.flexc}>
+          <h4>Section 5.0</h4>
+        </div>
+        <div className={Main.flexc}>
+          <h4>
+            <Move to="/cryptography">6.0 Cryptography and PKI</Move>
+          </h4>
+        </div>
       </div>
-      
 
       <div className={Main.miniflex}>
-      <div>
-        <Img fluid={props.data.front4.childImageSharp.fluid} />
+        <div>
+          <Img fluid={props.data.front4.childImageSharp.fluid} />
         </div>
 
         <div className={Main.lleft}>
           <h3>So. It has come to this</h3>
-          <p >Quarantined at home with a large stockpile of Swiss Cake rolls and nothing to do but study for the CompTIA Security+ exam. And by study I mean practice my Gatsby and React skills while making memes about it. You have been warned. </p>
+          <p>
+            Quarantined at home with a large stockpile of Swiss Cake rolls and
+            nothing to do but study for the CompTIA Security+ exam. And by study
+            I mean practice my Gatsby and React skills while making memes about
+            it. You have been warned.{" "}
+          </p>
         </div>
-       
-       
       </div>
-
-
- <div className={Main.miniflex}>
-      
-        <div className={Main.rright}>
-          <h3>Irrelevant</h3>
-          <p>Lots of information on the test is not relevant to the vast majority of people forced to take it as a job requirement. There is a big difference between working around sensitive documents and configuring an 802.1x remote RADIUS authentication server for your WPA2 Enterprise WiFi.  </p>
-          <h3>Less Secure</h3>
-          <p>By making the average test taker waste time memorizing irrelevant subjects, the CompTIA takes away time that could be spent learning relevant concepts.</p>
-        </div>
-
-
-       <div>
-        <Img fluid={props.data.front5.childImageSharp.fluid} />
-        </div>
-
-       
-      </div>
-
-
-
 
       <div className={Main.miniflex}>
+        <div className={Main.rright}>
+          <h3>Irrelevant</h3>
+          <p>
+            Lots of information on the test is not relevant to the vast majority
+            of people forced to take it as a job requirement. There is a big
+            difference between working around sensitive documents and
+            configuring an 802.1x remote RADIUS authentication server for your
+            WPA2 Enterprise WiFi.{" "}
+          </p>
+          <h3>Less Secure</h3>
+          <p>
+            By making the average test taker waste time memorizing irrelevant
+            subjects, the CompTIA takes away time that could be spent learning
+            relevant concepts.
+          </p>
+        </div>
 
-
-       <div>
-      <Img fluid={props.data.front6.childImageSharp.fluid} />
+        <div>
+          <Img fluid={props.data.front5.childImageSharp.fluid} />
+        </div>
       </div>
 
-      <div className={Main.lleft}>
-        <h3>Just for fun!</h3>
-        <p>This site is just me in quarantine studying for the Security+ exam and making memes about it. I am also using this as a chance to practice my Gatsby and React skills, clearly my meme game is already top notch. Don't take this seriously, except for the meme quality.</p>
-       
+      <div className={Main.miniflex}>
+        <div>
+          <Img fluid={props.data.front6.childImageSharp.fluid} />
+        </div>
+
+        <div className={Main.lleft}>
+          <h3>Just for fun!</h3>
+          <p>
+            This site is just me in quarantine studying for the Security+ exam
+            and making memes about it. I am also using this as a chance to
+            practice my Gatsby and React skills, clearly my meme game is already
+            top notch. Don't take this seriously, except for the meme quality.
+          </p>
+        </div>
       </div>
 
-
-    
-     
-    </div>
-
-
-    
-      
       <div className={Main.b}>
-        <h3>Exam catogery breakdown:</h3>
-      <Img fluid={props.data.front.childImageSharp.fluid} />
-      <p style={{ fontStyle: `italic`, fontSize: `.8rem`}}>These can be learned in any order. In fact, they are all kind of intertwined so you will be anyway. This site starts with section 4.0</p>
+        <h3>Exam category breakdown:</h3>
+        <Img fluid={props.data.front.childImageSharp.fluid} />
+        <p style={{ fontStyle: `italic`, fontSize: `.8rem` }}>
+          These can be learned in any order. In fact, they are all kind of
+          intertwined so you will be anyway. This site starts with section 4.0
+        </p>
       </div>
-
-      </div>
+    </div>
     <Footer />
   </Fragment>
 )
@@ -121,9 +133,8 @@ export default Index
 
 export const query = graphql`
   query {
-   front: file(relativePath: { eq: "objectives2.png" }) {
+    front: file(relativePath: { eq: "objectives2.png" }) {
       childImageSharp {
-       
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
@@ -132,7 +143,6 @@ export const query = graphql`
 
     front2: file(relativePath: { eq: "deets.png" }) {
       childImageSharp {
-       
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
@@ -141,47 +151,34 @@ export const query = graphql`
 
     front3: file(relativePath: { eq: "details.png" }) {
       childImageSharp {
-       
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
       }
     }
 
-
-
-  front4: file(relativePath: { eq: "distractedbw2.png" }) {
+    front4: file(relativePath: { eq: "distractedbw2.png" }) {
       childImageSharp {
-       
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
       }
     }
 
-
-
-  front5: file(relativePath: { eq: "highprice.png" }) {
+    front5: file(relativePath: { eq: "highprice.png" }) {
       childImageSharp {
-       
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
       }
     }
 
-
-
-  front6: file(relativePath: { eq: "studyor.png" }) {
-    childImageSharp {
-     
-      fluid(maxWidth: 1000) {
-        ...GatsbyImageSharpFluid
+    front6: file(relativePath: { eq: "studyor.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
       }
     }
-  }
-
-  
-
   }
 `
